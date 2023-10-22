abf026d73080bf666891294775474d36bb0b0821
Wayward Heart <91356680+WaywardHeart@users.noreply.github.com>
2023-10-22 04:58:00 -0500

    Optimize the path cache slightly
    In games that have a lot of files and an RTP is being loaded,
    this can save a couple seconds.
    
    This also keeps the cases of archives with lower priority from
    overriding the higher priority archives.

--- src/filesystem/filesystem.cpp
+++ src/filesystem/filesystem.cpp
@@ -355,6 +355,8 @@ void FileSystem::removePath(const char *path, bool reload) {
 struct CacheEnumData {
   FileSystemPrivate *p;
   std::stack<std::vector<std::string> *> fileLists;
+  std::set<std::string> seenDirs;
+  std::set<std::string> seenFiles;
 
 #ifdef __APPLE__
   iconv_t nfc2nfd;
@@ -409,6 +411,11 @@ static PHYSFS_EnumerateCallbackResult cacheEnumCB(void *d, const char *origdir,
 
   std::string mixedCase(fullPath);
 
+  /* If we've already seen this mixed-case path, then we don't need to reenumerate it 
+   * A different case could have new stuff, though */
+  if (data.seenDirs.count(mixedCase))
+    return PHYSFS_ENUM_OK;
+
   /* FileSystem::normalize ensures that paths are NFD when looking for files on macOS 
    * Unfortunately, there's no guarantee the path actually is that,
    * especially when loading files from archives,
@@ -424,14 +431,20 @@ static PHYSFS_EnumerateCallbackResult cacheEnumCB(void *d, const char *origdir,
     /* Create a new list for this directory */
     std::vector<std::string> &list = data.p->fileLists[lowerCase];
 
+    /* Record that we've seen this directory */
+    data.seenDirs.insert(mixedCase);
+
     /* Iterate over its contents */
     data.fileLists.push(&list);
     PHYSFS_enumerate(fullPath, cacheEnumCB, d);
     data.fileLists.pop();
-  } else {
+  } else if (!data.seenFiles.count(lowerCase)) {
     /* Get the file list for the directory we're currently
      * traversing and append this filename to it */
     std::vector<std::string> &list = *data.fileLists.top();
+
+    /* Only use the first one we found */
+    data.seenFiles.insert(lowerCase);
 
     char fn[256];
     strcpy(fn,fname);
