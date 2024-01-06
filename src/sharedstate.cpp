a6efa1bae20833ea8c47843473f04ab6ef8fce92
Nathan-MV <67521199+Nathan-MV@users.noreply.github.com>
2024-01-06 03:11:35 +0000

    fix 2

--- src/sharedstate.cpp
+++ src/sharedstate.cpp
@@ -119,7 +119,7 @@ struct SharedStatePrivate
 			fileSystem.addPath(config.patches[i].c_str());
 
 		/* Check if a game archive exists */
-		FILE *tmp = fopen(metaPath.c_str(), "rb");
+		FILE *tmp = fopen(metaPath, "rb");
 		if (tmp)
 		{
 			fileSystem.initializeArchiveMetadata(metaPath, config);
